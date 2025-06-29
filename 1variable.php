f54eb0bacd1571752dabf24c005ce519eb47805a
Sumaiya Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-06-29 22:54:53 +0600

    Add files via upload

---
+++ 1variable.php
@@ -0,0 +1,11 @@
+
+<?php
+//Variables are containers for storing information
+//Start with a $
+//For example: $name="abc";
+
+$name="Sumaiya";
+$age=22;
+echo "Hi everyone this is $name and her age is $age<br>";
+echo "$name is a good girl";
+?>
